0a77fc4818b4956cd418673ed09192bfa5ea45f0
Edward Guo <edward.h.guo@gmail.com>
2020-12-27 14:58:17 -0800

    Setup

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from '@emotion/styled'
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #444;
+`;
+
+const App = () => {
+  return (
+    <AppContainer>Hello World</AppContainer>
+  );
+}
+
+export default App;
