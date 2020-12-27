edb0a7d5e4678b08ff9c50ae7f96e1c78d56b531
Edward Guo <edward.h.guo@gmail.com>
2020-12-27 15:13:34 -0800

    Setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import styled from '@emotion/styled'
+import GenshinTracker from './GenshinTracker'
 
 const AppContainer = styled.div`
+  display: grid;
+  place-items: center;
   width: 100vw;
   height: 100vh;
   background-color: #222222;
+  color: #ededed;
 `;
 
 const App = () => {
   return (
-    <AppContainer>Hello World</AppContainer>
+    <AppContainer>
+      <GenshinTracker />
+    </AppContainer>
   );
 }
 
---
+++ src/components/GenshinTracker/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GenshinTracker = () => {
+  return (
+    <div>Genshin Tracker</div>
+  );
+};
+
+export default GenshinTracker;
